117426e5aa770164c4c64b050e3f6155978f9528
jawalton6616 <>
2021-06-03 21:37:51 +0000

    Added functionality to askForName function and askQuestion function

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,25 +3,31 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
-
+  // TODO 1.1b: Ask for candidate's name
+  candidateName = input.question("Please enter your name. ")
+  console.log(`Welcome ${candidateName}!`);
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //  
+  candidateAnswer = input.question(question);
+  if (candidateAnswer.toLowerCase().trim() === correctAnswer.toLowerCase().trim()){
+    console.log("Correct!");
+  }
+  else{
+    console.log("Incorrect");
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
